089b50f549b41c032749a8c871fc7e0695bb1055
Garira MOUNDI <mazougarira@yahoo.fr>
2020-10-17 15:42:05 +0200

    Creation of microservices architecture

---
+++ auth/src/main/java/org/projet_iwa/auth/api/Main.java
@@ -0,0 +1,11 @@
+package org.projet_iwa.auth.api;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "org.projet_iwa")
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Authentication Microservice");
+    }
+}
